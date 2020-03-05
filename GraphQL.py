1109e3f198d52b9d21b4bf49bad96bd6550948b0
Isabela Edilene <isabelaedilene@gmail.com>
2020-03-05 19:18:44 -0300

    Paginacao 100 rep. + arquivo csv

--- GraphQL.py
+++ GraphQL.py
@@ -1,32 +1,81 @@
-import requests
-import json
+from csv import writer
+from json import dumps
+from requests import post
 
-headers = {"Authorization": "key"}
+headers = {"Authorization": ""}
 
 
-def run_query(query): 
-    request = requests.post('https://api.github.com/graphql', json={'query': query}, headers=headers)
+def run_query(query_):
+    request = post(
+        "https://api.github.com/graphql", json={"query": query_}, headers=headers
+    )
     if request.status_code == 200:
         return request.json()
     else:
-        raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
-        
+        raise Exception(
+            "Query failed to run by returning code of {}. {}".format(
+                request.status_code, query_
+            )
+        )
+
+
 query = """
-query example {
-  search(query:"stars:>100",
-    type:REPOSITORY, first:5){
+query example{
+  search(query:"stars:>100", type:REPOSITORY, first:50{AFTER}){
+    pageInfo{
+        hasNextPage
+        endCursor
+    }
     nodes {
       ... on Repository {
         nameWithOwner
-        diskUsage
+        createdAt
+        updatedAt
+        pullRequests {
+          totalCount
+        }
+        releases {
+          totalCount
+        }
+        primaryLanguage {
+          name
+        }
+        totalIssues: issues{
+          totalCount
+        }  
+        totalClosesIssues: issues(states:CLOSED) {
+          totalCount
+        }      
       }
     }
   }
 }
 """
 
-result = run_query(query) 
-print(json.dumps(result, indent=4, sort_keys=True))
+finalQuery = query.replace("{AFTER}", "")
+result = run_query(finalQuery)
 
-# nameWithOwner = result["data"]["search"]["nodes"]  
-# print("nameWithOwner - {}".format(nameWithOwner))
+totalPages = 1
+currentEndCursor = result["data"]["search"]["pageInfo"]["endCursor"]
+hasNextPage = result["data"]["search"]["pageInfo"]["hasNextPage"]
+allResults = result["data"]["search"]["nodes"]
+
+while hasNextPage and totalPages < 20:
+    finalQuery = query.replace("{AFTER}", ', after: "%s"' % currentEndCursor)
+    result = run_query(finalQuery)
+    allResults += result["data"]["search"]["nodes"]
+
+    totalPages += 1
+    hasNextPage = result["data"]["search"]["pageInfo"]["hasNextPage"]
+    currentEndCursor = result["data"]["search"]["pageInfo"]["endCursor"]
+
+print(dumps(allResults, indent=4, sort_keys=True))
+
+first_row = allResults[0].keys()
+
+# Saving repositories' data to CSV file
+with open("repos.csv", "w") as csv_file:
+    csv = writer(csv_file)
+    csv.writerow(first_row)
+    for repo in allResults:
+        csv.writerow(repo.values())
